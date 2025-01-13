9fd2a7b956fe13ebd7f3a95b2db05ecb175f930f
GoodbyeNJN <cc@fuckwall.cc>
2025-01-13 23:53:31 +0800

    fix: fix webdav useProxy switch not work

--- app/utils/cloud/webdav.ts
+++ app/utils/cloud/webdav.ts
@@ -8,15 +8,18 @@ export function createWebDavClient(store: SyncStore) {
   const folder = STORAGE_KEY;
   const fileName = `${folder}/backup.json`;
   const config = store.webdav;
-  const proxyUrl =
-    store.useProxy && store.proxyUrl.length > 0 ? store.proxyUrl : undefined;
+  const useProxy = store.useProxy && store.proxyUrl.length > 0;
+  const pathPrefix = "/api/webdav";
+  const [webdavUrl, proxyUrl] = [config.endpoint, store.proxyUrl].map((url) =>
+    url.endsWith("/") ? url.slice(0, -1) : url,
+  );
 
   return {
     async check() {
       try {
-        const res = await fetch(this.path(folder, proxyUrl, "MKCOL"), {
-          method: "GET",
-          headers: this.headers(),
+        const res = await this.webdavFetch(folder, {
+          webdavMethod: "MKCOL",
+          httpMethod: "GET",
         });
         const success = [201, 200, 404, 405, 301, 302, 307, 308].includes(
           res.status,
@@ -35,9 +38,8 @@ export function createWebDavClient(store: SyncStore) {
     },
 
     async get(key: string) {
-      const res = await fetch(this.path(fileName, proxyUrl), {
-        method: "GET",
-        headers: this.headers(),
+      const res = await this.webdavFetch(fileName, {
+        webdavMethod: "GET",
       });
 
       console.log("[WebDav] get key = ", key, res.status, res.statusText);
@@ -50,9 +52,8 @@ export function createWebDavClient(store: SyncStore) {
     },
 
     async set(key: string, value: string) {
-      const res = await fetch(this.path(fileName, proxyUrl), {
-        method: "PUT",
-        headers: this.headers(),
+      const res = await this.webdavFetch(fileName, {
+        webdavMethod: "PUT",
         body: value,
       });
 
@@ -66,32 +67,49 @@ export function createWebDavClient(store: SyncStore) {
         authorization: `Basic ${auth}`,
       };
     },
-    path(path: string, proxyUrl: string = "", proxyMethod: string = "") {
-      if (path.startsWith("/")) {
-        path = path.slice(1);
-      }
 
-      if (proxyUrl.endsWith("/")) {
-        proxyUrl = proxyUrl.slice(0, -1);
-      }
+    webdavFetch(
+      input: string,
+      init: Omit<RequestInit, "method"> & {
+        httpMethod?: string;
+        webdavMethod: string;
+      },
+    ) {
+      const path = !input.startsWith("/") ? `/${input}` : input;
+      const { webdavMethod, httpMethod, ...options } = init;
 
+      let method;
       let url;
-      const pathPrefix = "/api/webdav/";
+      if (useProxy) {
+        method = httpMethod ?? webdavMethod;
 
-      try {
-        let u = new URL(proxyUrl + pathPrefix + path);
-        // add query params
-        u.searchParams.append("endpoint", config.endpoint);
-        proxyMethod && u.searchParams.append("proxy_method", proxyMethod);
-        url = u.toString();
-      } catch (e) {
-        url = pathPrefix + path + "?endpoint=" + config.endpoint;
-        if (proxyMethod) {
-          url += "&proxy_method=" + proxyMethod;
+        try {
+          const u = new URL(proxyUrl + pathPrefix + path);
+          u.searchParams.append("endpoint", webdavUrl);
+          webdavMethod && u.searchParams.append("proxy_method", webdavMethod);
+          url = u.toString();
+        } catch (e) {
+          url = pathPrefix + path + "?endpoint=" + webdavUrl;
+          if (webdavMethod) {
+            url += "&proxy_method=" + webdavMethod;
+          }
+        }
+      } else {
+        method = webdavMethod;
+
+        try {
+          let u = new URL(webdavUrl + path);
+          url = u.toString();
+        } catch (e) {
+          url = webdavUrl + path;
         }
       }
 
-      return url;
+      return fetch(url, {
+        ...options,
+        method,
+        headers: this.headers(),
+      });
     },
   };
 }
